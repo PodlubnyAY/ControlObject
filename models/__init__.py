c34a3aaaeb314d0f7711675967df3c6100eb738f
PodlubnyAY <andrewpdlbn@gmail.com>
2025-03-31 18:46:46 +0300

    update database path

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,15 @@
+import os
+from pathlib import Path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Настройка базы данных
-DATABASE_URL = "sqlite:///demo2.db"
-engine = create_engine(DATABASE_URL, echo=True)
-
+db_path = Path(os.getenv('APPDATA'), 'TTPOSU')
+db_path.mkdir(exist_ok=True)
+db_path /= 'database.db'
+DATABASE_URL = f"sqlite:///{db_path}"
+engine = create_engine(DATABASE_URL)
 Base = declarative_base()
-
 Session = sessionmaker(bind=engine)
 
 from . import users
